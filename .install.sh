a8d3456c76991e78a6e49c964e04aa293b98d20a
Arun Kumar <arunkakorp@gmail.com>
2024-09-06 15:39:45 +0000

    Made rvm install conditional

--- .install.sh
+++ .install.sh
@@ -6,7 +6,7 @@ if [[ ! -f /bin/fish ]]; then
     sudo apt install fish
 fi
 
-if [[ ! -d ~/.rvm ]]; then
+if [[ ! -f /usr/share/rvm/bin/rvm ]]; then
     gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     sudo apt-add-repository -y ppa:rael-gc/rvm
     sudo apt-get update
